c985b89803cb26184bd22249b060d58f25799243
Dawid Kaleta <dawid.kaleta@knightec.se>
2023-08-29 15:31:48 +0200

    Re-order CRUD-operations

--- src/controllers/post_controller.rs
+++ src/controllers/post_controller.rs
@@ -4,9 +4,9 @@ use rocket::serde::json::Json;
 
 use crate::models::post::Post;
 
-pub async fn create_post(_post: Data<'_>) -> Result<Status, Status> {
-    // Implement your create_post logic here
-    Ok(Status::Created)
+pub fn get_all_posts() -> Json<Vec<Post>> {
+    // Implement your get_all_posts logic here
+    Json(vec![])
 }
 
 pub fn get_post(post_id: i32) -> Option<Json<Post>> {
@@ -14,7 +14,7 @@ pub fn get_post(post_id: i32) -> Option<Json<Post>> {
     None
 }
 
-pub fn get_all_posts() -> Json<Vec<Post>> {
-    // Implement your get_all_posts logic here
-    Json(vec![])
+pub async fn create_post(_post: Data<'_>) -> Result<Status, Status> {
+    // Implement your create_post logic here
+    Ok(Status::Created)
 }
--- src/routes/post_routes.rs
+++ src/routes/post_routes.rs
@@ -10,9 +10,9 @@ pub fn routes() -> Vec<Route> {
     routes![create_post, get_post, get_all_posts]
 }
 
-#[post("/", data = "<_post>")]
-pub async fn create_post(_post: Data<'_>) -> Result<Status, Status> {
-    post_controller::create_post(_post).await
+#[get("/")]
+pub fn get_all_posts() -> Json<Vec<Post>> {
+    post_controller::get_all_posts()
 }
 
 #[get("/<post_id>")]
@@ -20,7 +20,7 @@ pub fn get_post(post_id: i32) -> Option<Json<Post>> {
     post_controller::get_post(post_id)
 }
 
-#[get("/")]
-pub fn get_all_posts() -> Json<Vec<Post>> {
-    post_controller::get_all_posts()
+#[post("/", data = "<_post>")]
+pub async fn create_post(_post: Data<'_>) -> Result<Status, Status> {
+    post_controller::create_post(_post).await
 }
