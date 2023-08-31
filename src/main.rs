5b71c6603fe602e8b00a1595ececf9eb71875ab5
Dawid Kaleta <dawid.kaleta@knightec.se>
2023-08-31 11:20:55 +0200

    Add secret from dotenv to use for db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use] extern crate rocket;
+use dotenv::dotenv;
+use std::env;
 
 mod routes;
 mod models;
@@ -6,6 +8,9 @@ mod controllers;
 
 #[launch]
 fn rocket() -> _ {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+
         rocket::build()
         .mount("/api/posts", routes::post_routes::routes())
 }
