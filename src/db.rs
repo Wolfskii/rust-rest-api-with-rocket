70e0bee93f4f52efa2b7d9dd1191895c7a98b074
Dawid Kaleta <dawid.kaleta@knightec.se>
2023-11-24 09:42:27 +0100

    Add creation of database name if missing on init

--- src/db.rs
+++ src/db.rs
@@ -16,13 +16,35 @@ pub async fn init_pool() -> Result<MySqlPool, Error> {
     // URL-encode the password
     let encoded_password = encode(&password);
 
-    // Build the database URL
+    // Build the connection URL (without specifying the database name initially)
+    let connection_url = format!(
+        "mysql://{}:{}@{}:{}/",
+        user, encoded_password, host, port
+    );
+
+    // Create a connection pool without specifying a database
+    let pool_without_db = MySqlPool::connect(&connection_url).await?;
+
+    // Check if the specified database exists
+    let database_exists: Option<i32> = sqlx::query_scalar(&format!("SELECT 1 FROM information_schema.schemata WHERE schema_name = '{}'", database_name))
+        .fetch_optional(&pool_without_db)
+        .await
+        .unwrap_or_default();
+
+    if database_exists.unwrap_or_default() == 0 {
+        // If the database does not exist, create it
+        sqlx::query(&format!("CREATE DATABASE IF NOT EXISTS {}", database_name))
+            .execute(&pool_without_db)
+            .await?;
+    }
+
+    // Build the full database URL now that the database exists
     let database_url = format!(
         "mysql://{}:{}@{}:{}/{}",
         user, encoded_password, host, port, database_name
     );
 
-    // Create a connection pool
+    // Create a connection pool with the specified database
     let pool = MySqlPool::connect(&database_url).await?;
 
     Ok(pool)
